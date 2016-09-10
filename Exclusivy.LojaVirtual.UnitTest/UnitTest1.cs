18a87235d7a7db8c0b65499d13bace806c19520e
exclusivy <lcpyes@hotmail.com>
2016-09-10 17:30:01 -0300

    inicial commit

---
+++ Exclusivy.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exclusivy.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestExclusivy
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
