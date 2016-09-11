b6bb44a2fe47932f2cef56bc25092ec1f47c567e
exclusivy <lcpyes@hotmail.com>
2016-09-10 22:26:17 -0300

    Entity

--- Exclusivy.LojaVirtual.Dominio/Entidade/Produto.cs
+++ Exclusivy.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -8,7 +8,7 @@ namespace Exclusivy.LojaVirtual.Dominio.Entidade
 {
     public class Produto
     {
-        public int ProdutoI { get; set; }
+        public int ProdutoId { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
